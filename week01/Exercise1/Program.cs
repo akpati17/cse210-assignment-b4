5fd3a9d19d6f6927a37998d2ab8b36380da05e49
akpati17 <akpatichijindu@gmail.com>
2026-07-06 23:38:21 +0100

    new

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name? ");
+        string first=Console.ReadLine();
+
+        Console.WriteLine("what is your last name? ");
+        string last=Console.ReadLine();
+
+        Console.WriteLine( $" Your is name {last}, {first}, {last}");
+
     }
 }
